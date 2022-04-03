99a0a11cdf78b3e6054292de93e6dbfb2323f2ed
Eri Schon <erischon@gmail.com>
2022-04-03 09:18:17 +0200

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
@@ -5,8 +7,11 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that support using email instead of username."""
+    """
+    Custom user model that support using email instead of username.
+    """
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
